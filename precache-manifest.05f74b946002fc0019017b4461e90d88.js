28cd483f9ad25f18ad0c8d82f6b4515b3585cbb6
Leo Belyi <leonid@ac93.org>
2019-02-20 20:22:25 -0500

    Updates

---
+++ precache-manifest.05f74b946002fc0019017b4461e90d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23f701aa914f11c9c4c8",
+    "url": "/index/static/css/main.fe0369a1.chunk.css"
+  },
+  {
+    "revision": "23f701aa914f11c9c4c8",
+    "url": "/index/static/js/main.23f701aa.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/index/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b2ad6c4cc5b9c40f0899",
+    "url": "/index/static/js/runtime~main.b2ad6c4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/index/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "16d8b954ae5f4f48f5207d8517a56ab4",
+    "url": "/index/index.html"
+  }
+];
