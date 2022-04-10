e27ff98a9a71122bddd1c261c8898dbbf2b015fb
Leo Belyi <leonid.belyi@mycase.com>
2022-04-10 10:35:24 -0700

    Remove boilerplate, style better, and add name

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Leo Belyi </h1>
+        <b>Software Engineer</b>
+        <p>PCT Alum</p>
+        <i>Heartthrob</i>
       </header>
     </div>
   );
